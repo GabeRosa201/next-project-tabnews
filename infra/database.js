6e26dfbbb67c6350dcc670a78de0dd3fc0e819ed
Gabriel Rosa <gabrielrosamelo@hotmail.com>
2025-04-21 13:28:54 +0000

    add conexao com o banco de dados

--- infra/database.js
+++ infra/database.js
@@ -1,1 +1,13 @@
-export default {};
+import { Client } from "pg";
+
+async function query(queryObejct) {
+  const client = new Client();
+  await client.connect();
+  const result = await client.query(queryObejct);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
