0d2491296fbe68fb7c1e9396dd8f4fe52a56b3c2
Gabriel Rosa <gabrielrosamelo@hotmail.com>
2025-04-21 14:05:25 +0000

    add database.js

--- infra/database.js
+++ infra/database.js
@@ -1,7 +1,13 @@
 import { Client } from "pg";
 
 async function query(queryObejct) {
-  const client = new Client();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_P,
+  });
   await client.connect();
   const result = await client.query(queryObejct);
   await client.end();
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,8 @@
 import database from "../../../../infra/database";
 
-function status(req, res) {
-  console.log(database);
+async function status(req, res) {
+  const result = await database.query(" SELECT 1 * 1 AS multiplicacao");
+  console.log(result.rows);
   res.status(200).json("Alunos acima de media");
 }
 
