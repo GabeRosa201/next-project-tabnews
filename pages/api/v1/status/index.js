e670b65e54abafa728aebd68fbfc534eab770798
Gabriel Rosa <gabrielrosamelo@hotmail.com>
2025-08-23 20:03:08 +0000

    add retorno do horario dos dados e testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,14 @@
 import database from "infra/database";
 
 async function status(req, res) {
+  // const uptadedAt = Date.now() -> Obtem a data no formato de timestapm
+  const uptadedAt = new Date().toISOString();
+
   const result = await database.query(" SELECT 1 * 1 AS multiplicacao");
-  console.log(result.rows);
-  res.status(200).json("Alunos acima de media");
+  // console.log(result.rows);
+  res.status(200).json({
+    updated_at: uptadedAt,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
 test("GET to api/v1/status must return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+  console.log(responseBody);
 });
